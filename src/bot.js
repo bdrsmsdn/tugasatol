d23562018ec220de77150e19b431dc55bb8ac50f
bdrsmsdn <badrasam7@gmail.com>
2021-08-10 01:59:13 +0700

    hehe

--- src/bot.js
+++ src/bot.js
@@ -17,7 +17,11 @@ const base64 = require('base64topdf');
 bot.on(['/start', '/hello'], (msg) => {
   msg.reply.text(
     `Hai ${msg.chat.first_name}!✨
-Saya adalah bot yang dibuat oleh @bdrsmsdn untuk memenuhi salah satu Tugas Besar Mata Kuliah Aplikasi Teknologi Online.
+Saya adalah Agro-BOT yang dibuat oleh t.me/bdrsmsdn untuk memenuhi salah satu Tugas Besar Mata Kuliah Aplikasi Teknologi Online. Saya dibuat menggunakan bahasa Node.js dengan bantuan framework dari Telebot. Saya berfungsi untuk mengetahui ketinggian suatu daerah berdasar lokasi yang anda kirim, dan memberi tahu zona iklim dan tanaman apa yang cocok dibudayakan. API yang digunakan di bot ini antara lain:
+
+1. https://core.telegram.org/api
+2. https://api.open-elevation.com/api/v1/lookup
+3. https://badra.my.id/api/agro
 
 Ketik /help untuk melihat menu.`,
     { asReply: true }
@@ -30,14 +34,7 @@ Ketik /help untuk melihat menu.`,
 // });
 
 bot.on(['/help', '/menu'], (msg) => {
-  msg.reply.text(
-    `Halo, berikut adalah beberapa menu yang tersedia:
-1. /downloader untuk melihat list menu Downloader
-2.
-3.
-4.`,
-    { asReply: true }
-  );
+  msg.reply.text(`Halo, silakan kirim lokasi untuk memulai. BTW kita punya fitur downloader juga ko, langsung saja /downloader yaa.`, { asReply: true });
   return console.log(color('[EXEC]'), color(new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString('id'), 'yellow'), color(`/help`), 'from', color(`${msg.chat.first_name}`));
 });
 
@@ -66,90 +63,100 @@ bot.on('photo', (msg) => {
 //   msg.reply.text(`Silakan kirim lokasi untuk melihat informasi sekolah di sekitarmu`, { asReply: true });
 // });
 
-bot.on('location', (msg) => {
-  const chatId = msg.chat.id;
-  var location = msg.location;
-  console.log(`${location.latitude},${location.longitude}`);
-  axios
-    .get(`https://api.open-elevation.com/api/v1/lookup?locations=${location.latitude},${location.longitude}`)
-    .then(function (response) {
-      let res = response.data.results[0];
-      var optio = {
-        method: 'GET',
-        url: `https://badra.my.id/api/agro?alt=${res.elevation}`,
-      };
+bot.on('/agro', (msg) => {
+  msg.reply.text('Hai silakan kirim lokasi ya!', { asReply: true });
+  while (
+    bot.on('location', (msg) => {
+      const chatId = msg.chat.id;
+      var location = msg.location;
+      console.log(`${location.latitude},${location.longitude}`);
       axios
-        .request(optio)
-        .then(function (respons) {
-          let resw = respons.data.results;
-          let xi = `Anda berada pada ketinggian: ${res.elevation} mdpl, ${resw.zone}
-
-${resw.description}
-${resw.plant}
-`;
-          bot.sendMessage(chatId, `${xi}`, { asReply: true });
+        .get(`https://api.open-elevation.com/api/v1/lookup?locations=${location.latitude},${location.longitude}`)
+        .then(function (response) {
+          let res = response.data.results[0];
+          var optio = {
+            method: 'GET',
+            url: `https://badra.my.id/api/agro?alt=${res.elevation}`,
+          };
+          axios
+            .request(optio)
+            .then(function (respons) {
+              let resw = respons.data.results;
+              let xi = `Anda berada pada ketinggian: ${res.elevation} mdpl, ${resw.zone}
+  
+  ${resw.description}
+  ${resw.plant}
+  `;
+              bot.sendMessage(chatId, `${xi}`, { asReply: true });
+            })
+            .catch(function (error) {
+              bot.sendMessage(chatId, `Terjadi kesalahan di API 2!`, { asReply: true });
+            });
         })
         .catch(function (error) {
-          bot.sendMessage(chatId, `Terjadi kesalahan di API 2!`, { asReply: true });
+          bot.sendMessage(chatId, `Terjadi kesalahan di API Geocode!`, { asReply: true });
         });
+      return console.log(color('[EXEC]'), color(new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString('id'), 'yellow'), color(`/agro`), 'from', color(`${msg.chat.first_name}`));
     })
-    .catch(function (error) {
-      bot.sendMessage(chatId, `Terjadi kesalahan di API Geocode!`, { asReply: true });
-    });
-  return console.log(color('[EXEC]'), color(new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString('id'), 'yellow'), color(`/agro`), 'from', color(`${msg.chat.first_name}`));
+  );
 });
 
-// bot.on('location', (msg) => {
-//   const chatId = msg.chat.id;
-//   var location = msg.location;
-//   console.log(`${location.latitude},${location.longitude}`);
-//   var options = {
-//     method: 'GET',
-//     url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/reverse',
-//     params: {
-//       lat: `${location.latitude}`,
-//       lon: `${location.longitude}`,
-//       'accept-language': 'en',
-//       polygon_threshold: '0.0',
-//     },
-//     headers: {
-//       'x-rapidapi-key': '33cddd5088msh54605b0ab723661p1863b7jsn4a751612ee44',
-//       'x-rapidapi-host': 'forward-reverse-geocoding.p.rapidapi.com',
-//     },
-//   };
-//   axios
-//     .request(options)
-//     .then(function (response) {
-//       let res = response.data.address;
-//       var optio = {
-//         method: 'GET',
-//         url: `https://api-sekolah-indonesia.herokuapp.com/sekolah/s?sekolah=${res.village}`,
-//       };
-//       axios
-//         .request(optio)
-//         .then(function (response) {
-//           let xi = `Informasi sekolah di sekitar lokasi ${res.village}\n\n`;
-//           let resw = response.data;
-//           let resq = resw.dataSekolah;
-//           for (let i = 1; i < resq.length; i++) {
-//             xi += `${i}. Nama sekolah: ${resq[i].sekolah}
-// Status: ${resq[i].bentuk}${resq[i].status}
-// Alamat: ${resq[i].alamat_jalan}
-// NPSN: ${resq[i].npsn}
-// Maps: https://www.google.co.id/maps/@${resq[i].lintang},${resq[i].bujur}
-// \n========================\n`;
-//           }
-//           bot.sendMessage(chatId, `${xi}`, { asReply: true });
-//         })
-//         .catch(function (error) {
-//           bot.sendMessage(chatId, `Terjadi kesalahan di API Sekolah!`, { asReply: true });
-//         });
-//     })
-//     .catch(function (error) {
-//       bot.sendMessage(chatId, `Terjadi kesalahan di API Geocode!`, { asReply: true });
-//     });
-//   return console.log(color('[EXEC]'), color(new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString('id'), 'yellow'), color(`/sekolah`), 'from', color(`${msg.chat.first_name}`));
-// });
+bot.on('/sekolah', (msg) => {
+  msg.reply.text('Hai silakan kirim lokasi ya!', { asReply: true });
+  while (
+    bot.on('location', (msg) => {
+      const chatId = msg.chat.id;
+      var location = msg.location;
+      console.log(`${location.latitude},${location.longitude}`);
+      var options = {
+        method: 'GET',
+        url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/reverse',
+        params: {
+          lat: `${location.latitude}`,
+          lon: `${location.longitude}`,
+          'accept-language': 'en',
+          polygon_threshold: '0.0',
+        },
+        headers: {
+          'x-rapidapi-key': '33cddd5088msh54605b0ab723661p1863b7jsn4a751612ee44',
+          'x-rapidapi-host': 'forward-reverse-geocoding.p.rapidapi.com',
+        },
+      };
+      axios
+        .request(options)
+        .then(function (response) {
+          let res = response.data.address;
+          var optio = {
+            method: 'GET',
+            url: `https://api-sekolah-indonesia.herokuapp.com/sekolah/s?sekolah=${res.village}`,
+          };
+          axios
+            .request(optio)
+            .then(function (response) {
+              let xi = `Informasi sekolah di sekitar lokasi ${res.village}\n\n`;
+              let resw = response.data;
+              let resq = resw.dataSekolah;
+              for (let i = 1; i < resq.length; i++) {
+                xi += `${i}. Nama sekolah: ${resq[i].sekolah}
+  Status: ${resq[i].bentuk}${resq[i].status}
+  Alamat: ${resq[i].alamat_jalan}
+  NPSN: ${resq[i].npsn}
+  Maps: https://www.google.co.id/maps/@${resq[i].lintang},${resq[i].bujur}
+  \n========================\n`;
+              }
+              bot.sendMessage(chatId, `${xi}`, { asReply: true });
+            })
+            .catch(function (error) {
+              bot.sendMessage(chatId, `Terjadi kesalahan di API Sekolah!`, { asReply: true });
+            });
+        })
+        .catch(function (error) {
+          bot.sendMessage(chatId, `Terjadi kesalahan di API Geocode!`, { asReply: true });
+        });
+      return console.log(color('[EXEC]'), color(new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString('id'), 'yellow'), color(`/sekolah`), 'from', color(`${msg.chat.first_name}`));
+    })
+  );
+});
 
 // bot.on('/cat', (msg) => {
 //   const chatId = msg.chat.id;
